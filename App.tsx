6eba66763ddca9438d81215c753804635abbb211
Stanley Jeong <stanley.jeong@shopify.com>
2023-04-19 11:25:45 -0700

    navigation added

--- App.tsx
+++ App.tsx
@@ -1,126 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, useEffect } from 'react';
-import {
-  TextInput,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  SafeAreaView,
-  FlatList,
-  VirtualizedList,
-  StyleSheet,
-  Text,
-  View,
-  Switch
-} from 'react-native';
-import { cats, dogs } from './breeds';
-import AnimalBreed from './components/AnimalBreed';
-const globalStyles = require('./styles')
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const CATS = 'cats'
-const DOGS = 'dogs'
+const Stack = createStackNavigator();
+import BreedsApp from './components/BreedsApp';
+import DetailsScreen from './screens/DetailsScreen';
 
 export default function App() {
-  const [search, setSearch] = useState('');
-  const [filteredData, setFilteredData] = useState<any[]>();
-  const [selectedAnimal, setSelectedAnimal] = useState(CATS);
-  const [switchEnabled, setSwitchEnabled] = useState(false);
-
-  useEffect(() => {
-    let animalsSet = selectedAnimal === CATS ? cats : dogs;
-    const filtered = animalsSet.filter((animal) => animal.breed.toLowerCase().includes(search.toLowerCase()))
-    setFilteredData(filtered)
-  }, [search, selectedAnimal])
-
-  useEffect(() => {
-    setSelectedAnimal(() => switchEnabled ? DOGS : CATS)
-  }, [switchEnabled])
-
-  const getItem = (_data: unknown, index: number): any => filteredData;
-
-  const getItemCount = (_data: unknown) => 50;
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <SafeAreaView>
-        <KeyboardAvoidingView
-          style={styles.keyboardView}
-          behavior={Platform.OS === 'ios' ? "padding" : "height"}
-        >
-          <TextInput
-            style={styles.searchInput}
-            placeholder='Search'
-            value={search}
-            // onChangeText={(newText) => setSearch(newText)}
-            onChangeText={setSearch} // same exact effect
-          />
-          <View style={styles.switchToggle}>
-            <Text style={globalStyles.textColor}>Cats</Text>
-            <Switch
-              value={switchEnabled}
-              onValueChange={() => setSwitchEnabled((state) => !state)}
-              trackColor={{false: "#0D415A", true: '#E04A31'}}
-              thumbColor={switchEnabled ? '#F7EEA7' : '#f4f3f4'}
-            />
-            <Text style={globalStyles.textColor}>Dogs</Text>
-          </View>
-        </KeyboardAvoidingView>
-
-        <Text style={{color: '#E04A31', textAlign: 'right', paddingHorizontal: 20, fontWeight: 'bold', fontSize: 20}}>{switchEnabled ? 'Dogs' : 'Cats'}</Text>
-        {/* <VirtualizedList
-          data={filteredData}
-          initialNumToRender={4}
-          renderItem={({ item }) => {
-            return <AnimalBreed animal={item} title={`${item.breed}`} />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Breeds"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#e1154f',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Breeds"
+          component={BreedsApp}
+          options={{
+            title: "Breeds",
+            headerStyle: {
+              backgroundColor: '#f4511e'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 24
+            }
           }}
-          keyExtractor={(item) => item.breed}
-          getItemCount={getItemCount}
-          getItem={getItem}
-        /> */}
-
-        {/* <ScrollView>
-          {filteredData?.map((item, index) => {
-            return <AnimalBreed animal={item} title={`${item.breed}`} />
-          })}
-        </ScrollView> */}
-
-        <FlatList
-          style={{marginBottom: 50, paddingBottom: 100}}
-          data={filteredData}
-          renderItem={({ item }) => {
-            return <AnimalBreed animal={item} title={`${item.breed}`} />
-          }}
-          keyExtractor={(item) => item.breed}
         />
-      </SafeAreaView>
-
-    </View>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{
+            title: 'Details'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0D415A',
-    justifyContent: 'center',
-  },
-  searchInput: {
-    backgroundColor: '#F7EEA7',
-    borderRadius: 10,
-    padding: 10,
-    flex: 1.5,
-    fontSize: 20,
-  },
-  keyboardView: {
-    marginTop: 100,
-    flexDirection: 'row',
-    paddingHorizontal: 20,
-  },
-  switchToggle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    flex: 1,
-  },
-});
--- components/AnimalBreed.tsx
+++ components/AnimalBreed.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Feature from './Feature';
-const globalStyles = require('../styles');
+import { TouchableOpacity } from 'react-native';
+import AnimalCard from './AnimalCard';
 
 interface AnimalBreedProps {
   animal: any;
   title: string;
+  navigation: any;
 }
 
-function AnimalBreed({ animal, title }: AnimalBreedProps) {
+export default function AnimalBreed({ animal, title, navigation }: AnimalBreedProps ) {
   const features = Object.keys(animal);
 
   const numericFeatures = features.filter((feature, idx, array) => (array.indexOf(feature) === idx) && feature !== 'breed');
@@ -17,44 +17,10 @@ function AnimalBreed({ animal, title }: AnimalBreedProps) {
   const average = len > 0 ? (fullScore / len).toFixed(1) : 0;
 
   return(
-    <View style={styles.container}>
-      <View style={styles.titleRow}>
-        <Text style={styles.breedName}>{title}</Text>
-        <Text style={styles.breedName}>Avg: {average}</Text>
-      </View>
-
-      <View style={styles.featuresContainer}>
-        {numericFeatures
-          .map((feature, index) => <Feature key={`${index} ${feature}`} feature={feature} score={animal[feature]}/>
-        )}
-      </View>
-    </View>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('Details', { title, average, numericFeatures, animal })}
+    >
+      <AnimalCard title={title} average={average} numericFeatures={numericFeatures} animal={animal} />
+    </TouchableOpacity>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  featuresContainer: {
-    marginBottom: 10,
-    backgroundColor: '#26B5A1',
-    borderRadius: 10,
-    paddingVertical: 4,
-  },
-  titleRow: {
-    display: 'flex',
-		justifyContent: 'space-between',
-    flexDirection: 'row',
-    marginBottom: 4
-  },
-  breedName: {
-    fontFamily: 'Helvetica',
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#DEC027',
-  },
-})
-
-export default AnimalBreed;
